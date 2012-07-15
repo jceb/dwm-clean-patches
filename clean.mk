7985bd186db2ebdab792ee9cf040e5d0ba28db45
Jan Christoph Ebersbach <jceb@e-jc.de>
2012-07-15 22:18:13 +0200

    improve determining the release version

--- clean.mk
+++ clean.mk
@@ -1,12 +1,11 @@
 include config.mk
-VVERSION=$(shell hg qpop -a; hg identify -r $(shell hg identify -i) | awk '{print $$2}')
+VVERSION=$(shell hg identify -r $(shell hg qpop -a &> /dev/null; hg identify -i) | awk '{print $$2}')
 
 .PHONY: all build clean cleanconfigh
 
 all: build
 
 build: .hg/patches .hg/patches/README clean.sh
-	mkdir -p $(VVERSION)
 	./clean.sh $(VVERSION) $(args)
 
 clean:
